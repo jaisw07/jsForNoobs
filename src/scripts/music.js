fea261f917e6f1775c6f47e7f72824c23d01521e
jaisw07 <jaiswalshrey07@gmail.com>
2024-11-09 05:13:39 +0530

    v2.2: added music script with array of song metadata

---
+++ src/scripts/music.js
@@ -0,0 +1,20 @@
+let musicList = [
+    {
+        name: "Do Pal",
+        audioPath: "../assets/songs/DoPal.mp3",
+        albumArt: "../assets/images/ab67616d0000b27396fa496a2cfc301430589024.jpeg.jpg",
+        artist: "Madan Mohan",
+    },
+    {
+        name: "Main Yahan Hoon",
+        audioPath: "../assets/songs/MainYahanHoon",
+        albumArt: "../assets/images/ab67616d0000b27396fa496a2cfc301430589024.jpeg.jpg",
+        artist: "Udit Narayan",
+    },
+    {
+        name: "Tere Liye",
+        audioPath: "../assets/songs/TereLiye.mp3",
+        albumArt: "../assets/images/ab67616d0000b27396fa496a2cfc301430589024.jpeg.jpg",
+        artist: "Lata Mangeshkar",
+    },
+]
