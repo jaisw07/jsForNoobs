de75e7023b37afda32702376894c79e9a080c8a3
jaisw07 <jaiswalshrey07@gmail.com>
2024-11-09 05:07:26 +0530

    v2.1: script for variables added

---
+++ src/scripts/script.js
@@ -0,0 +1,20 @@
+const albArt = document.querySelector("#albumArt")
+const songName = document.querySelector("#songName")
+const songArtist = document.querySelector("#songArtist")
+//
+const seekBar = document.querySelector("#seekBar")
+const currTime = document.querySelector("#currentTime")
+const totalTime = document.querySelector("#totalTime")
+//
+const play = document.querySelector("#playBtn")
+const prev = document.querySelector("#prevBtn")
+const fwd = document.querySelector("#fwdBtn")
+const rep = document.querySelector("#repBtn")
+const shuffle = document.querySelector("#shufBtn")
+//
+const volIcon = document.querySelector("#volIcon")
+const volBar = document.querySelector("#volBar")
+const hamburger = document.querySelector("#hamburger")
+//
+const playlistView = document.querySelector(".playlist")
+const songTile = document.querySelector(".songTile")
