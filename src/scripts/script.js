affdd03fe8138db0b5756cdda0752ea43ccaff1b
jaisw07 <jaiswalshrey07@gmail.com>
2024-11-09 05:49:31 +0530

    v2.3: song play functionality added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,3 +18,36 @@ const hamburger = document.querySelector("#hamburger")
 //
 const playlistView = document.querySelector(".playlist")
 const songTile = document.querySelector(".songTile")
+//control variables
+let timer;
+let indexSong = 0;
+let isPlaying = false;
+let song = document.createElement("audio");
+
+// Event Listeners
+play.addEventListener("click", playSong);
+
+// Load Songs
+function loadSong(indexSong) {
+    songName.innerHTML = musicList[indexSong].name;
+    song.src = musicList[indexSong].audioPath;
+    albArt.src = musicList[indexSong].albumArt;
+    songArtist.innerHTML = musicList[indexSong].artist;
+    song.load();
+}
+
+loadSong(indexSong);
+
+// Play Song
+function playSong() {
+    if(!isPlaying) {
+        song.play();
+        isPlaying = true;
+        play.src = '<i class="fa-solid fa-pause"></i>';
+    }
+    else{
+        song.pause();
+        isPlaying = false;
+        play.src = '<i class="fa-solid fa-play"></i>';
+    }
+}
