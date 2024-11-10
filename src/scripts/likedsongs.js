cf2c8e52280550172ede4acad1e22d950ee2b56e
sg-0x <shashwat.gaur1001@gmail.com>
2024-11-11 03:13:50 +0530

    edited likedsongs

--- src/scripts/likedsongs.js
+++ src/scripts/likedsongs.js
@@ -1,4 +1,26 @@
-let name = "john"
-let surname = "doe"
+//Liked Songs
+const toggleLike = (likeBtnSelector, checkboxSelector) => {
+    const likeBtn = document.querySelectorAll(likeBtnSelector);
+    const checkbox = document.querySelectorAll(checkboxSelector);
+    console.log("CALL")
 
-console.log()
+    likeBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const isLiked = !checkbox[0].checked;
+            checkbox[0].checked = isLiked;
+            btn.classList.toggle("liked", isLiked);
+            console.log("Okay")
+        });
+    });
+};
+
+toggleLike('.likebtn', '.checkbox');
+
+
+function storeLikedSongs() {
+    const likedSongs = [];
+    document.querySelectorAll('.song-item.liked').forEach(song => {
+        likedSongs.push(song.getAttribute('data-song-id'));
+    });
+    localStorage.setItem('likedSongs', JSON.stringify(likedSongs));
+}
